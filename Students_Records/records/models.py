68cb3d9bbba0b0c63e3e15243d0f378491f66303
Birajlama3 <145580509+Birajlama3@users.noreply.github.com>
2026-05-13 15:48:10 +0545

    changes done

--- Students_Records/records/models.py
+++ Students_Records/records/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Records(models.Mmodel):
+    name = models.CharField( max_length=50)
+    task = models.CharField()
--- Students_Records/records/urls.py
+++ Students_Records/records/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .import views
 
 urlpatterns =[
-    path('records/', views.records, name='records'),
+    path('', views.records, name='records'),
 ]
