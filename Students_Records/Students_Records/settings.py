7983329d2916a963efa077dae6377793ce77470a
Birajlama3 <145580509+Birajlama3@users.noreply.github.com>
2026-05-20 11:29:09 +0545

    configured static files

--- Students_Records/Students_Records/settings.py
+++ Students_Records/Students_Records/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,4 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR/ 'static'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "records/static"
+]
+
+STATIC_ROOT = BASE_DIR/ 'staticfiles'
