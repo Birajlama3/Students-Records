56a4ddc7cd14989163d589e90035d074f8d9e9ed
Birajlama3 <145580509+Birajlama3@users.noreply.github.com>
2026-05-26 15:39:12 +0545

    Fix database config for Render

--- Students_Records/Students_Records/settings.py
+++ Students_Records/Students_Records/settings.py
@@ -80,12 +80,16 @@ WSGI_APPLICATION = 'Students_Records.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-import dj_database_url
 import os
+import dj_database_url
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 DATABASES = {
     'default': dj_database_url.config(
-    default=f'sqlite:///{BASE_DIR / "db.sqlite3"}'    )
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}'
+    )
 }
 # DATABASES = {
 #     'default': {
